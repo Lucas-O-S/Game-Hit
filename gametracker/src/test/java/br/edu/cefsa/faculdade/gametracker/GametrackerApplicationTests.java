a0103c2a8807633ca1d099029b81b57b9253a861
Lucas-O-S <lucasoscomercial@gmail.com>
2025-05-06 09:48:12 -0300

    first commit

---
+++ gametracker/src/test/java/br/edu/cefsa/faculdade/gametracker/GametrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.cefsa.faculdade.gametracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GametrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
