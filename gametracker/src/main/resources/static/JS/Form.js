252a897f10302cdcabadc8dc0632a7b87efaa7ed
Lucas-O-S <Lucasoscomercial@gmail.com>
2025-05-14 20:46:29 -0300

    melhorias

--- gametracker/src/main/resources/static/JS/Form.js
+++ gametracker/src/main/resources/static/JS/Form.js
@@ -1,8 +1,13 @@
-function ImageAutolader(){
-  const operacao = document.getElementById("operacao");
-  
+function ErrorAlert(error) {
+  Swal.fire({
+    icon: 'error',
+    title: 'Erro!',
+    text: error
+  });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOM carregado!");
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM carregado!");
+  const error = document.getElementById("error").value;
+  ErrorAlert(error);
+})
